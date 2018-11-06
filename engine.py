abe76b374707bddbce4bece21d69631bea6dc594
fejoa <ryanvanherel@gmail.com>
2018-11-06 13:45:39 +1300

    Added rectangle class; defined rooms, horizontal and vertical tunnels
    Made two rooms, connected with a horizontal tunnel.
    Put player inside the first room.

--- engine.py
+++ engine.py
@@ -14,6 +14,16 @@ con = tcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 colour_dark_wall = tcod.Color(0, 30, 0)
 colour_dark_ground = tcod.Color(20, 60, 20)
 
+
+class Rect:
+    # A rectangle on the map, used to characterise a room
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+
 class Tile:
     # A tile of the map and its porperties
     def __init__(self, blocked, block_sight = None):
@@ -22,6 +32,7 @@ class Tile:
         # By default, if a tile is blocked, it also blocks sight
         block_sight = blocked if block_sight is None else None
         self.block_sight = block_sight
+
 
 class Object:
     # This is a generic object: the player, a monster, an item, the toilet...
@@ -50,20 +61,50 @@ class Object:
         else:
             tcod.console_put_char(con, self.x, self.y, ' ', tcod.BKGND_NONE)
 
+
+def create_room(room):
+    global map
+    # Go through the tiles in the rectangle and make them passable
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            map[x][y].blocked = False
+            map[x][y].block_sight = False
+
+
 def make_map():
     global map
 
-    # Fill map with "unblocked" tiles
+    # Fill map with "blocked" tiles
     map = [
-        [Tile(False) for y in range (MAP_HEIGHT)]
+        [Tile(True) for y in range (MAP_HEIGHT)]
         for x in range(MAP_WIDTH)
     ]
 
-    # Place two test tiles
-    map[30][22].blocked = True
-    map[30][22].block_sight = True
-    map[50][22].blocked = True
-    map[50][22].block_sight = True
+    room1 = Rect(20, 15, 10, 15)
+    room2 = Rect(50, 15, 10, 15)
+    create_room(room1)
+    create_room(room2)
+
+    player.x = 25
+    player.y = 23
+
+    create_h_tunnel(25, 55, 23)
+
+
+def create_h_tunnel(x1, x2, y):
+    global map
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+
+def create_v_tunnel(y1, y2, x):
+    global map
+    # vertical tunnel
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
 
 def render_all():
     global colour_light_wall
@@ -83,12 +124,13 @@ def render_all():
                 else:
                     tcod.console_set_char_background(con, x, y, colour_dark_ground, tcod.BKGND_SET)
 
-    #draw all objects in the list
+    # Draw all objects in the list
     for object in objects:
         object.draw()
 
     # Blit the contents of con to the root console
     tcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
 
 def get_key_event(turn_based = None):
     if turn_based:
@@ -98,6 +140,7 @@ def get_key_event(turn_based = None):
         # Real-time gameplay; don't wait for a player's keystoke
         key = tcod.console_check_for_keypress()
     return key
+
 
 def handle_keys():
     global player_x, player_y
@@ -124,6 +167,7 @@ def handle_keys():
     elif tcod.console_is_key_pressed(tcod.KEY_RIGHT):
         player.move(1, 0)
 
+
 def initialize_game():
     # Set up font
     font_path = 'arial10x10.png'
@@ -139,18 +183,15 @@ def initialize_game():
 
     make_map()
 
+
 player = Object(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, '@', tcod.white)
 npc = Object(SCREEN_WIDTH // 2 - 5, SCREEN_HEIGHT // 2, '@', tcod.yellow)
 objects = [npc, player]
 
+
 def main():
     initialize_game()
-
-
-
     exit_game = False
-
-
 
     while not tcod.console_is_window_closed() and not exit_game:
         render_all()
